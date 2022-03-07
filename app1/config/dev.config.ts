320a7ce83d6d4ebf819baafc0ba8af74f9ef1083
liangsky <640634387@qq.com>
2022-03-07 09:29:43 +0800

    qiankun

--- app1/config/dev.config.ts
+++ app1/config/dev.config.ts
@@ -23,6 +23,12 @@ if (process.env.NODE_ENV === 'development') {
         changeOrigin: true,
         secure: false,
       },
+      '/sub-zxkp-h5/mf-': {
+        target: 'http://localhost:8001',
+        changeOrigin: true,
+        secure: false,
+      },
+      //http://localhost:6001/sub-zxkp-h5/mf-dep_vendors-node_modules_core-js_index_js.f8014813.async.js
       /*'/sub-demo1': {
         target: 'http://localhost:8001',
         changeOrigin: true,
